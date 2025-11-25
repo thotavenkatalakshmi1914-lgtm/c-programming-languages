9a915a744ee25b395901e295a414e4410b9ad694
thotavenkatalakshmi1914-lgtm <thotavenkatalakshmi1914@gmail.com>
2025-11-25 11:40:52 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,12 @@
+/*program for to check age is eligible or not*/
+#include<stdio.h>
+int main(){
+	int age;
+	printf("enter your age:");
+	scanf("%d",&age);
+	if(age)
+	printf("you are eligible for vote");
+	else
+	printf("you are not eligible for vote");
+	return 0;
+}
